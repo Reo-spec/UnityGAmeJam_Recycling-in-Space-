956c84fb1e4ec0e9c9e0cc997ba88735ef084f26
Noma shunsuke <ca01264026@st.kawahara.ac.jp>
2026-07-21 19:40:36 +0900

    プレイヤー移動(WASD)と問題解決

---
+++ Assets/Player/PlayerScript.cs
@@ -0,0 +1,46 @@
+///
+///プレイヤーを制御するクラス
+///
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerScript : MonoBehaviour
+{
+    //プレイヤーの移動速度
+    float moveSpeed = 1.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        Vector3 move = Vector3.zero;
+
+        if (Keyboard.current.wKey.isPressed)
+        {
+            move.z = 1.0f;
+        }
+        if (Keyboard.current.sKey.isPressed)
+        {
+            move.z = -1.0f;
+        }
+        if (Keyboard.current.dKey.isPressed)
+        {
+            move.y = 1.0f;
+        }
+        if (Keyboard.current.aKey.isPressed)
+        {
+            move.y = -1.0f;
+        }
+        transform.Translate(move * moveSpeed * Time.deltaTime);
+    }
+}
