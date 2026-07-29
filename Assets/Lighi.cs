a4fff841f4f0b168cc5405daef03fd628d42c4c4
siraisi yuuga <ca01264015@st.kawahara.ac.jp>
2026-07-29 18:08:14 +0900

    イベントの最終追加

--- Assets/Lighi.cs
+++ Assets/Lighi.cs
@@ -1,38 +1,47 @@
 using UnityEngine;
 
-
-
-
 public class LightColorManager : MonoBehaviour
-
 {
-
-    public Light targetLight;
-
-    public EventManager eventManager;
-
-
-
+    [SerializeField] private EventManager eventManager;
+    [SerializeField] private Light targetLight;
+   
     void Update()
-
     {
 
-        if (eventManager.CurrentState == EventManager.EventState.MeteorImpact)
-
+        void Update()
         {
+            if (eventManager == null)
+            {
+                Debug.Log("eventManager‚ª–¢İ’è");
+                return;
+            }
 
+            if (targetLight == null)
+            {
+                Debug.Log("targetLight‚ª–¢İ’è");
+                return;
+            }
+
+            if (eventManager.IsMeteorEvent)
+            {
+                targetLight.color = Color.red;
+                targetLight.intensity = 100f; // –¾‚é‚­‚·‚é
+            }
+            else
+            {
+                targetLight.color = Color.white;
+                targetLight.intensity = 1f; // ’Êí
+            }
+        }
+
+
+        if (eventManager.IsMeteorEvent)
+        {
             targetLight.color = Color.red;
-
         }
-
         else
-
         {
-
             targetLight.color = Color.white;
-
         }
-
     }
-
 }
